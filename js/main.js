a17df82ce79b51c25d8a0ee843202d76f9d1f841
pabloxe123321 <pabloxe.acevedo@gmail.com>
2023-08-24 19:56:20 -0400

    waw

--- js/main.js
+++ js/main.js
@@ -1,25 +1,69 @@
+const cervezas=[
+   {nombre:"Budweiser", precio: 2100},
+   {nombre:"Cusqueña", precio: 2300},
+   {nombre:"Royal guard", precio: 1800}]
+const piscos=[
+   {nombre:"Lapostolle", precio: 14000},
+   {nombre:"Mistral", precio: 6700}]
+const vinos=[
+   {nombre:"Misioneros", precio: 8600},
+   {nombre:"Gato", precio: 5000}];
+const bebidas=[
+   {nombre:"Coca cola", precio: 3000},
+   {nombre:"Fanta", precio: 3100},
+   {nombre:"Pap", precio: 2900}]
 
+const carrito= [];
 
+const producto= [
+   {nombre:"cerveza", productos: cervezas},
+   {nombre:"pisco", productos: piscos},
+   {nombre:"vino", productos: vinos},
+   {nombre:"bebida", productos: bebidas},
 
-let nombre = prompt("Buenas, ingresanos tu nombre");
-let edad = parseInt(prompt("Bienvenido "+nombre+ " indicanos tu edad"));
- while (isNaN(edad)){
-    edad = parseInt(prompt("Respuesta no valida, porfavor indicanos tu edad nuevamente"));
- }
-let comida = prompt("Que es lo que mas te gusta comer??");
-let respuesta="";
+];
 
-for (let i = 1 ; i<=3 ; i++){
- let pregunta = prompt("Intento numero "+i+" de 3, cual es la capital de chile??" )
- if (pregunta != "santiago"){
-  respuesta= "No"
-  alert(respuesta)
- } else{
-    respuesta= "Correcto!!!"
-    alert(respuesta)
- }
+while (true) {
+   const seleccion = parseInt(prompt("Ingrese el numero de lo que desea comprar:"+"\n 1. Cervezas"+"\n 2. Piscos"+"\n 3. Vinos"+"\n 4. Bebidas"+ "\n Para ver Total carrito, ingresa 0"))
+   
+   if (seleccion === 0){
+      break;
+   }
+
+   if(!isNaN(seleccion)){
+      const eleccion = producto[seleccion - 1]
+      
+      if (eleccion){
+         let lista = "";
+         eleccion.productos.forEach((element, index) => {
+            const contador = index + 1;
+            lista += contador + ". "+ element.nombre + " $" + element.precio + "\n";
+         });
+            
+         const opcion = parseInt(prompt("Selecciona tu producto"+ "\n" +  lista));
+
+         if (opcion >= 1 && opcion <= eleccion.productos.length){
+            carrito.push(eleccion.productos[opcion -1]);
+         } else{
+            alert("El numero ingresado es invalido, intentelo nuevamente")
+         }
+      }else
+         alert("Eliga una opcion del 1 al 4")
+   }
+}
+if (carrito.length === 0) {
+      alert ("No hay nada en el carrito")
+}else{
+let compra="";
+carrito.forEach((element, index) => { 
+      const contador = index + 1;
+      compra += contador + ". "+ element.nombre + " $" + element.precio + "\n";
+});
+let total=carrito.reduce((total, element) => total + element.precio, 0);
+alert("Estas por comprar:" + "\n" + compra + "\n Su total es de: "+ total + "$");
 }
 
-console.log(nombre+" tiene "+edad+" y le gusta comer "+comida)
- 
+
+
+
 
