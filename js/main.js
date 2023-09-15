55d0b9689d9df5734d5e21bd0417e32ce42bb76d
pabloxe123321 <pabloxe.acevedo@gmail.com>
2023-09-15 19:10:49 -0300

    correccion pipipi

--- js/main.js
+++ js/main.js
@@ -1,21 +1,48 @@
 let cartItems = [];
-    let total = 0;
+let total = 0;
+function loadCartFromLocalStorage() {
+    const storedCartItems = JSON.parse(localStorage.getItem("cartItems"));
+    const storedTotal = parseFloat(localStorage.getItem("cartTotal"));
+    
+    if (storedCartItems) {
+        cartItems = storedCartItems;
+    }
+    
+    if (!isNaN(storedTotal)) {
+        total = storedTotal;
+    }
+    
+    updateCartDisplay();
+}
+
+function saveCartToLocalStorage() {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    localStorage.setItem("cartTotal", total.toFixed(2));
+}
 
 function addToCart(productName, productPrice) {
     cartItems.push({ name: productName, price: productPrice });
     total += productPrice;
 
+    saveCartToLocalStorage();
 
     updateCartDisplay();
-        }
+}
+
+function clearCart() {
+    cartItems = [];
+    total = 0;
+
+    saveCartToLocalStorage();
+
+    updateCartDisplay();
+}
 
 function updateCartDisplay() {
     const cartList = document.getElementById("cart-items");
     const cartTotal = document.getElementById("cart-total");
-
-
+    
     cartList.innerHTML = "";
-
 
     cartItems.forEach((item) => {
         const li = document.createElement("li");
@@ -23,6 +50,11 @@ function updateCartDisplay() {
         cartList.appendChild(li);
     });
 
- 
     cartTotal.innerText = total.toFixed(0);
 }
+
+window.onload = function () {
+    loadCartFromLocalStorage();
+    const clearCartButton = document.getElementById("clear-cart-button");
+    clearCartButton.addEventListener("click", clearCart);
+};
